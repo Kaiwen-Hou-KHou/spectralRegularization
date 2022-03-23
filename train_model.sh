1374037bfe9c249bf1db8eebeb7aedde69f14476
grwip <gr_wip@hotmail.com>
2022-03-23 12:51:08 -0400

    fixed small bug in train_model.sh

--- train_model.sh
+++ train_model.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SOURCE_DIR=~/repos/spectralRegularization
+ENV_DIR=~
 module load python/3.8
-source $SOURCE_DIR/env/bin/activate
+source $ENV_DIR/env/bin/activate
 python $SOURCE_DIR/train_model.py "$@"
